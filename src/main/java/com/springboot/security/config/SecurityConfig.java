55119ea1e55075bcec737f4299335a08aeacab53
maheshkn123 <nayakmahesh380@gmail.com>
2024-07-21 22:34:09 +0530

    Initialize Project

---
+++ src/main/java/com/springboot/security/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.springboot.security.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/public/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .and()
+            .httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
